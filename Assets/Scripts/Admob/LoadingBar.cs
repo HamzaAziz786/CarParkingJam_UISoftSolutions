f2c23dc87a0d3f23ea8132342c00a709bf219b43
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-10-13 06:23:58 -0700

    asd

--- Assets/Scripts/Admob/LoadingBar.cs
+++ Assets/Scripts/Admob/LoadingBar.cs
@@ -10,7 +10,7 @@ public class LoadingBar : MonoBehaviour
     private void OnEnable()
     {
         LoadingBarImg.transform.localScale = new Vector3(0,1,1);
-        LoadingBarImg.transform.DOScaleX(1, 8f).OnComplete(() => HidePanell());
+        LoadingBarImg.transform.DOScaleX(1, 10f).OnComplete(() => HidePanell());
     }
     public void HidePanell()
     {
