0618e4c0b43eb4fe74c0b6d64735b7f50c1e7306
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-11-07 10:42:08 +0500

    LevelsSetting_ScriptCars

--- Assets/Scripts/Script/LevelScripts/LevelInfo.cs
+++ Assets/Scripts/Script/LevelScripts/LevelInfo.cs
@@ -9,12 +9,18 @@ public class LevelInfo : MonoBehaviour
     public int Car_Crossed;
     public GameObject Confettii;
     public GameObject[] Hurdles;
+    public GameObject[] Cars;
+
+    public int Hurdles_IncreaseSize;
+    public int Car_IncreaseSize;
     public static LevelInfo instance;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
+        EnableHurdles();
+        Car_Size();
     }
     private void OnEnable()
     {
@@ -36,7 +42,15 @@ public class LevelInfo : MonoBehaviour
         for (int i = 0; i < Hurdles.Length; i++)
         {
             //Hurdles[i].gameObject.SetActive(true);
-            Hurdles[i].transform.DOScale(1, Random.Range(0.1f, .5f)).SetEase(Ease.OutBounce);
+            Hurdles[i].transform.DOScale(Hurdles_IncreaseSize, Random.Range(0.1f, .5f)).SetEase(Ease.OutBounce);
+        }
+    }
+    public void Car_Size()
+    {
+        for (int i = 0; i < Cars.Length; i++)
+        {
+            //Hurdles[i].gameObject.SetActive(true);
+            Cars[i].transform.DOScale(Car_IncreaseSize, Random.Range(0.1f, .5f)).SetEase(Ease.OutBounce);
         }
     }
     private void Add(int num)
